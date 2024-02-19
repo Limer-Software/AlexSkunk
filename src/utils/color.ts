e8a42f240422e1564d134bfab9875cf012747da2
RobotoSkunk <contact@robotoskunk.com>
2024-02-18 18:36:31 -0600

    Replaced " with ' character

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -16,7 +16,7 @@
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import Canvas from "@napi-rs/canvas";
+import Canvas from '@napi-rs/canvas';
 
 
 export default class Color
