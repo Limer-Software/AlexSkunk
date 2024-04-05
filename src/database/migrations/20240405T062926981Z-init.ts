f6d8829a508db238037305fd0f2a5f450ef826f0
RobotoSkunk <contact@robotoskunk.com>
2024-04-05 01:11:52 -0600

    Minor fixes to migration-create.ts

--- src/database/migrations/20240405T062926981Z-init.ts
+++ src/database/migrations/20240405T062926981Z-init.ts
@@ -1,6 +1,6 @@
 /*
 	Alex Skunk, a discord bot.
-	Copyright (C) 2024  <name of author>
+	Copyright (C) 2024 RobotoSkunk <contact@robotoskunk.com>
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published
