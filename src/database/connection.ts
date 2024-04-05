9640c96f84a8522193ce42e2ab47298c96ccea56
RobotoSkunk <contact@robotoskunk.com>
2024-04-04 23:59:26 -0600

    Setted up migrations system

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -17,9 +17,12 @@
 */
 
 import { Pool } from 'pg';
-import { Kysely, PostgresDialect } from 'kysely';
+import { Kysely, Migrator, PostgresDialect } from 'kysely';
 
 import DatabaseSchema, { DatabaseSchemaType } from './schema';
+import { ContextMigrationProvider } from './migrations/provider';
+
+import * as migrations from './migrations';
 
 
 class Database
@@ -34,6 +37,11 @@ class Database
 	 */
 	private db: DatabaseSchema;
 
+	/**
+	 * Kysely migrator
+	 */
+	private migrator: Migrator;
+
 
 	/**
 	 * Create a new database connection pool based on environment variables
@@ -45,11 +53,16 @@ class Database
 			host: process.env.DB_HOST,
 			password: process.env.DB_PASSWORD,
 			port: Number.parseInt(process.env.DB_PORT),
-			user: process.env.DB_USER
+			user: process.env.DB_USER,
 		});
 
 		const dialect = new PostgresDialect({ pool: this.pool });
 		this.db = new Kysely<DatabaseSchemaType>({ dialect });
+
+		this.migrator = new Migrator({
+			db: this.db,
+			provider: new ContextMigrationProvider(migrations, 'pg')
+		})
 	}
 
 	/**
@@ -58,13 +71,57 @@ class Database
 	public async testConnection(): Promise<void>
 	{
 		try {
-			await this.pool.query('SELECT NOW()');
+			// await this.pool.query('SELECT NOW()');
+
+			await this.db
+				.selectFrom('users')
+				.select([ b => b.fn.count('id').as('count') ])
+				.execute();
+
 			console.log('Connected to database.');
 		} catch (error) {
 			console.error('Failed to connect to database.');
 			console.error(error);
 			process.exit(1);
 		}
+	}
+
+
+	/**
+	 * Just a wrapper of migrateTo but with try/catch compatibility.
+	 */
+	public async tryMigrateTo(migration: string)
+	{
+		const { error, results } = await this.migrator.migrateTo(migration);
+		
+		if (error) {
+			throw error;
+		}
+
+		if (!results) {
+			throw new Error('An unknown error ocurred while migrating.');
+		}
+
+		return results;
+	}
+
+
+	/**
+	 * Just a wrapper of migrateToLatest but with try/catch compatibility.
+	 */
+	public async tryMigrateToLatest()
+	{
+		const { error, results } = await this.migrator.migrateToLatest();
+		
+		if (error) {
+			throw error;
+		}
+
+		if (!results) {
+			throw new Error('An unknown error ocurred while migrating.');
+		}
+
+		return results;
 	}
 
 	/**
